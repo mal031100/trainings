b5043475cdedb78925326a763674252b1faa1aa6
mal031100 <88300591+mal031100@users.noreply.github.com>
2021-08-19 16:29:19 +0700

    NDHlam

---
+++ javascript/main.js
@@ -0,0 +1,31 @@
+// đăng nhập đăng xuất
+const login = document.querySelector('.js-login')
+const register = document.querySelector('.js-regis')
+const apc = document.querySelector('#register')
+
+function showregis(){
+    apc.classList.remove('open')
+}
+
+function hideregis(){
+    apc.classList.add('open')
+}
+
+register.addEventListener('click',showregis);
+
+login.addEventListener('click', hideregis);
+
+// welcome
+const loginwel = document.querySelector('.js-login-wel')
+const wel = document.querySelector('.js-out')
+const home = document.querySelector('#wel')
+
+function showwel(){
+    home.classList.remove('mal')
+}
+function hidewel(){
+    home.classList.add('mal')
+}
+
+loginwel.addEventListener('click',showwel);
+wel.addEventListener('click',hidewel);
---
+++ javascript/main1.js
@@ -0,0 +1,32 @@
+var objPeople = [
+	{ // Object @ 0 index
+		account: "nguyen dac hoang lam",
+		password: "lam"
+	},
+	{ // Object @ 1 index
+		account: "lam",
+		password: "mal"
+	},
+	{ // Object @ 2 index
+		account: "ndhl",
+		password: "lam"
+	}
+
+]
+
+function getInfo() {
+	var account = document.getElementById('acc').value
+	var password = document.getElementById('pass').value
+
+	for(var i = 0; i < objPeople.length; i++) {
+		// check is user input matches account and password of a current index of the objPeople array
+		if(account == objPeople[i].account && password == objPeople[i].password) {
+			alert(  "WELCOME"+ " " + account )
+			// stop the function if this is found to be true
+			return
+		}
+	}
+	alert("incorrect username or password")
+}
+
+
